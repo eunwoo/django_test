05838b2288bf2257ff9ebf5ff84bf5aadf986a0e
seokmin <soos3121@gmail.com>
2022-02-15 17:00:43 +0900

    feat : main 화면 추가

--- main/models.py
+++ main/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+# 하나만 생성 가능
+# 현장 등록
+class Field(models.Model):
+    title = models.CharField(max_length=90)  # 공사명
+    callNum = models.CharField(max_length=20)  # 현장 전화번호
+    address = models.TextField()  # 현장 주소
+    orderer = models.CharField(max_length=30)  # 발주자 성함
+    constructComp = models.CharField(max_length=40)  # 공사 회사
+    supervision = models.CharField(max_length=40)  # 감리 및 CM 업체 명
+    startDay = models.DateField()  # 공사 시작일
+    endDay = models.DateField()  # 공사 종료일
+
+    def __str__(self):
+        return self.title
+
+
+# 전문건설업체 관리자 연락처
+class ConstructManager(models.Model):
+    name = models.CharField(max_length=30)  # 공사 관리자 성함
+    belong = models.CharField(max_length=30)  # 공사 관리자 소속
+    phone = models.CharField(max_length=20)  # 공사 관리자 연락처
+    field_id = models.ForeignKey(Field, on_delete=models.CASCADE)  # 공사 관리자 소속 현장
+
+    def __str__(self):
+        return self.name
+
+
+# 관리대상 조립가설기자재 종류
+class EquipmentTypes(models.Model):
+    type = models.CharField(max_length=30)  # 종류 (강관 비계, 시스템 비계, 시스템 동바리 중 한 개)
+    field_id = models.ForeignKey(Field, on_delete=models.CASCADE)  # 공사 관리자 소속 현장
+
+    def __str__(self):
+        return self.type
+
+
+# 설치 위치 명
+class InstallLocate(models.Model):
+    class1 = models.CharField(max_length=30)  # 구분 1
+    class2 = models.CharField(max_length=30)  # 구분 2
+    class3 = models.CharField(max_length=30)  # 구분 3
+
+    def __str__(self):
+        return self.class1 + " " + self.class2 + " " + self.class3
+
+
+# 문서 등록
+class DocsFile(models.Model):
+    file = models.FileField(upload_to="docs/")  # 파일
+    filename = models.CharField(max_length=100)  # 파일명
+    type = models.CharField(max_length=30)  # 파일 타입
+
+    def __str__(self):
+        return self.filename
--- main/views.py
+++ main/views.py
@@ -1,5 +1,3 @@
-from django.http import HttpResponse
-from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
