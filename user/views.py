dac3358fc70cab3eaa4188725c4c0ac9d6728e41
seokmin <soos3121@gmail.com>
2022-02-15 14:09:54 +0900

    feat : 회원가입 연결

--- user/views.py
+++ user/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from . import forms
+
+
+def usertype(request):
+    return render(request, "auth/usertype.html")
+  
+def agreeForUser(request):
+    return render(request, "auth/agreeForUser.html")
+  
+def registerUser(request):
+    if request.method == "POST":
+        form = forms.UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("user:login")
+    else:
+        form = forms.UserForm()
+    return render(request, "auth/registerUser.html", {"form": form})
+
+def agreeForAdmin(request):
+    return render(request, "auth/agreeForAdmin.html")
+  
+def registerAdmin(request):
+    if request.method == "POST":
+        form = forms.UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("user:login")
+    else:
+        form = forms.UserForm()
+    return render(request, "auth/registerAdmin.html", {"form": form})
