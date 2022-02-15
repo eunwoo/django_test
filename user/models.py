6021b82ecaa1046aa27eb94787fd7d56622017d7
seokmin <soos3121@gmail.com>
2022-02-15 14:09:00 +0900

    feat : User 모델 추가

--- user/models.py
+++ user/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+# Create model for user registration
+class CustomUser(AbstractUser):
+    name = models.CharField(max_length=30)
+    class1 = models.CharField(max_length=60)
+    class2 = models.CharField(max_length=60)
+    class3 = models.CharField(max_length=60)
+    phone = models.CharField(max_length=20)
+    signImage = models.ImageField(upload_to='signature/', blank=True)
+    register = models.BooleanField(default=False)
+    first_name = None
+    last_name = None
