3d2a2be4e131f868addbb005dfbb05bea53b3b89
seokmin <soos3121@gmail.com>
2022-02-15 14:41:13 +0900

    fix : Admin 연결 수정

--- user/views.py
+++ user/views.py
@@ -5,10 +5,12 @@ from . import forms
 
 def usertype(request):
     return render(request, "auth/usertype.html")
-  
+
+
 def agreeForUser(request):
     return render(request, "auth/agreeForUser.html")
-  
+
+
 def registerUser(request):
     if request.method == "POST":
         form = forms.UserForm(request.POST)
@@ -19,15 +21,17 @@ def registerUser(request):
         form = forms.UserForm()
     return render(request, "auth/registerUser.html", {"form": form})
 
+
 def agreeForAdmin(request):
     return render(request, "auth/agreeForAdmin.html")
-  
+
+
 def registerAdmin(request):
     if request.method == "POST":
-        form = forms.UserForm(request.POST)
+        form = forms.AdminForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect("user:login")
     else:
-        form = forms.UserForm()
+        form = forms.AdminForm()
     return render(request, "auth/registerAdmin.html", {"form": form})
