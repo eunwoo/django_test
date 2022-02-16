70558907fc0fe725cea0bec8e5ccf79e3e3b77ab
seokmin <soos3121@gmail.com>
2022-02-16 17:01:18 +0900

    feat : system manager 기능 추가

--- system_manager/urls.py
+++ system_manager/urls.py
@@ -4,6 +4,30 @@ from . import views
 
 app_name = "system_manager"
 
+
 urlpatterns = [
+    path("apply_field/", views.apply_field, name="apply_field"),  # 현장등록
+    path("apply_locate/", views.apply_locate, name="apply_locate"),  # 설치위치명 등록
+    path("apply_document/", views.apply_document, name="apply_document"),  # 문서 등록
+    path(
+        "user_management/", views.user_management, name="user_management"
+    ),  # 사용자 등록 및 삭제
+    path("delete_user/<int:pk>/", views.delete_user, name="delete_user"),  # 사용자 삭제
+    path(
+        "register_user/<int:pk>/", views.register_user, name="register_user"
+    ),  # 사용자 등록
+    path(
+        "apply_document_template/<str:type>/",
+        views.apply_document_template,
+        name="apply_document_template",
+    ),  # 문서 템플릿 등록
+    path(
+        "upload_documents/<str:type>/", views.upload_documents, name="upload_documents"
+    ),  # 문서 업로드
+    path(
+        "delete_documents/<int:pk>/<str:type>",
+        views.delete_documents,
+        name="delete_documents",
+    ),  # 문서 삭제
     path("", views.index, name="index"),
 ]
--- system_manager/views.py
+++ system_manager/views.py
@@ -1,37 +1,111 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
+from user.models import CustomUser
+from .models import Field, ConstructManager, EquipmentTypes, InstallLocate, DocsFile
+from .forms import FieldForm
+
 
 @login_required(login_url="/user/login/")
 def index(request):
-    if not request.user.is_system_manager: # 시스템 매니저만 접근 가능
+    if not request.user.is_system_manager:  # 시스템 매니저만 접근 가능
         return redirect("main:home")
     return render(request, "system_manager/main.html")
 
 
 # 현장등록
+@login_required(login_url="/user/login/")
 def apply_field(request):
-    if not request.user.is_system_manager: # 시스템 매니저만 접근 가능
+    if not request.user.is_system_manager:  # 시스템 매니저만 접근 가능
         return redirect("main:home")
-    pass
+    form = FieldForm()
+    equipments = EquipmentTypes.objects.all()
+    return render(
+        request,
+        "system_manager/apply_field.html",
+        {"form": form, "equipments": equipments},
+    )
 
 
 # 설치위치명 등록
+@login_required(login_url="/user/login/")
 def apply_locate(request):
-    if not request.user.is_system_manager: # 시스템 매니저만 접근 가능
+    if not request.user.is_system_manager:  # 시스템 매니저만 접근 가능
         return redirect("main:home")
-    pass
+    return render(request, "system_manager/apply_locate.html")
 
 
 # 문서 등록
+@login_required(login_url="/user/login/")
 def apply_document(request):
-    if not request.user.is_system_manager: # 시스템 매니저만 접근 가능
+    if not request.user.is_system_manager:  # 시스템 매니저만 접근 가능
         return redirect("main:home")
-    pass
+    return render(request, "system_manager/apply_document.html")
+
+
+@login_required(login_url="/user/login/")
+def apply_document_template(request, type):
+    if not request.user.is_system_manager:  # 시스템 매니저만 접근 가능
+        return redirect("main:home")
+    files = DocsFile.objects.filter(type=type)
+    return render(
+        request,
+        "system_manager/apply_document_template.html",
+        {"type": type, "files": files},
+    )
+
+
+@login_required(login_url="/user/login/")
+def upload_documents(request, type):
+    if not request.user.is_system_manager:  # 시스템 매니저만 접근 가능
+        return redirect("main:home")
+    files = request.FILES.getlist("file")
+    for file in files:
+        f = DocsFile(file=file, type=type, filename=file.name)
+        f.save()
+    return redirect("system_manager:apply_document_template", type=type)
+
+
+@login_required(login_url="/user/login/")
+def delete_documents(request, pk, type):
+    if not request.user.is_system_manager:  # 시스템 매니저만 접근 가능
+        return redirect("main:home")
+    DocsFile.objects.get(pk=pk).delete()
+    return redirect("system_manager:apply_document_template", type=type)
 
 
 # 사용자 등록 및 삭제
+@login_required(login_url="/user/login/")
 def user_management(request):
-    if not request.user.is_system_manager: # 시스템 매니저만 접근 가능
+    if not request.user.is_system_manager:  # 시스템 매니저만 접근 가능
         return redirect("main:home")
-    pass
+    await_users = CustomUser.objects.filter(is_system_manager=False, register=False)
+    registered_users = CustomUser.objects.filter(is_system_manager=False, register=True)
+    return render(
+        request,
+        "system_manager/user_management.html",
+        {"await_users": await_users, "registered_users": registered_users},
+    )
+
+
+@login_required(login_url="/user/login/")
+def delete_user(request, pk):
+    if not request.user.is_system_manager:  # 시스템 매니저만 접근 가능
+        return redirect("main:home")
+    if request.method == "POST":
+        user = CustomUser.objects.get(pk=pk)
+        user.delete()
+        return redirect("system_manager:user_management")
+    return redirect("main:home")
+
+
+@login_required(login_url="/user/login/")
+def register_user(request, pk):
+    if not request.user.is_system_manager:  # 시스템 매니저만 접근 가능
+        return redirect("main:home")
+    if request.method == "POST":
+        user = CustomUser.objects.get(pk=pk)
+        user.register = True
+        user.save()
+        return redirect("system_manager:user_management")
+    return redirect("main:home")
