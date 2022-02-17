ab0c2ad767de74ca4068309ed91988886edcba4a
seokmin <soos3121@gmail.com>
2022-02-17 15:26:33 +0900

    feat : 설치위치 등록 (미완성)

--- system_manager/views/locate_views.py
+++ system_manager/views/locate_views.py
@@ -1,9 +1,31 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
+from ..models import LocateClass, InstallLocate
+
 
 @login_required(login_url="/user/login/")
 def apply_locate(request):
     if not request.user.is_system_manager:  # 시스템 매니저만 접근 가능
         return redirect("main:home")
-    return render(request, "system_manager/apply_locate.html")
+    class1 = LocateClass.objects.filter(type="class1")
+    class2 = LocateClass.objects.filter(type="class2")
+    class3 = LocateClass.objects.filter(type="class3")
+    install_locate = InstallLocate.objects.all()
+    return render(
+        request,
+        "system_manager/apply_locate.html",
+        {
+            "class1": class1,
+            "class2": class2,
+            "class3": class3,
+            "install_locate": install_locate,
+        },
+    )
+
+
+@login_required(login_url="/user/login/")
+def locate_class(request, pk):
+    if not request.user.is_system_manager:  # 시스템 매니저만 접근 가능
+        return redirect("main:home")
+    return redirect("main:home")
