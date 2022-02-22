2271c9d3c2a8b7c801cfbb70ba10c59475efc031
seokmin <soos3121@gmail.com>
2022-02-22 16:59:56 +0900

    style : 주석 추가

--- work/views/safety_views.py
+++ work/views/safety_views.py
@@ -22,14 +22,18 @@ def create_safety(request):
             return redirect("work:update_safety", safety.docNum)
     else:
         form = GeneralManagerSafetyReportForm()
-    last_doc = SafetyReport.objects.last()
-    construct_bills = DocsFile.objects.filter(type__contains="구조 계산서")
-    detail_drawings = DocsFile.objects.filter(type__contains="시공상셰도면")
 
+    # 문서 번호 로드
+    last_doc = SafetyReport.objects.last()
     if not last_doc:
         docNum = 1
     else:
         docNum = last_doc.docNum + 1
+
+    # 관련 문서 로드
+    construct_bills = DocsFile.objects.filter(type__contains="구조 계산서")
+    detail_drawings = DocsFile.objects.filter(type__contains="시공상셰도면")
+
     return render(
         request,
         "work/safety/create_safety.html",
