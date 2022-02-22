1c19786beaf984e2874a3e8981630e8e8781415b
seokmin <soos3121@gmail.com>
2022-02-22 16:56:51 +0900

    feat : 구조 안전성 신고서 제작 및 수정 기눙 추가

--- work/views/safety_views.py
+++ work/views/safety_views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
+from system_manager.models import DocsFile
+from ..models import SafetyReport
+from ..forms.safety_forms import GeneralManagerSafetyReportForm
+
 
 @login_required(login_url="/user/login/")
 def safety(request):
@@ -9,12 +13,51 @@ def safety(request):
 
 @login_required(login_url="/user/login/")
 def create_safety(request):
-    return render(request, "work/safety/create_safety.html")
+    if request.method == "POST":
+        form = GeneralManagerSafetyReportForm(request.POST)
+        if form.is_valid():
+            safety = form.save(commit=False)
+            safety.writerId = request.user
+            safety.save()
+            return redirect("work:update_safety", safety.docNum)
+    else:
+        form = GeneralManagerSafetyReportForm()
+    last_doc = SafetyReport.objects.last()
+    construct_bills = DocsFile.objects.filter(type__contains="구조 계산서")
+    detail_drawings = DocsFile.objects.filter(type__contains="시공상셰도면")
+
+    if not last_doc:
+        docNum = 1
+    else:
+        docNum = last_doc.docNum + 1
+    return render(
+        request,
+        "work/safety/create_safety.html",
+        {
+            "docNum": docNum,
+            "form": form,
+            "construct_bills": construct_bills,
+            "detail_drawings": detail_drawings,
+        },
+    )
 
 
 @login_required(login_url="/user/login/")
 def update_safety(request, pk):
-    pass
+    if request.method == "POST":
+        form = GeneralManagerSafetyReportForm(request.POST)
+        if form.is_valid():
+            safety = form.save(commit=False)
+            safety.save()
+            return redirect("work:update_safety", safety.docNum)
+    doc = SafetyReport.objects.get(docNum=pk)
+    form = GeneralManagerSafetyReportForm(instance=doc)
+    return render(request, "work/safety/create_safety.html", {"form": form})
+
+
+@login_required(login_url="/user/login/")
+def require_sign(request, docNum):
+    return render(request, "work/safety/update_safety.html", {"docNum": docNum})
 
 
 @login_required(login_url="/user/login/")
