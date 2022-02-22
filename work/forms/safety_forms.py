0dd8ba604256e764f650613fce95559f33c21824
seokmin <soos3121@gmail.com>
2022-02-22 16:51:09 +0900

    feat : safety_forms 제작

---
+++ work/forms/safety_forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+from ..models import SafetyReport
+
+
+class GeneralManagerSafetyReportForm(forms.ModelForm):
+    class Meta:
+        model = SafetyReport
+        fields = ["docNum", "date", "title", "constructType", "text", "writerId"]
