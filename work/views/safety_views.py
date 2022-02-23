17920c59861cc119d545f95d5cfa605d9369f71c
seokmin <soos3121@gmail.com>
2022-02-23 16:45:27 +0900

    feat : 메일 전송 추가

--- work/views/safety_views.py
+++ work/views/safety_views.py
@@ -1,14 +1,31 @@
 from django.contrib.auth.decorators import login_required
+from django.http import Http404, JsonResponse
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
 
 from system_manager.models import DocsFile
+from user.models import CustomUser
+
 from ..models import SafetyReport
 from ..forms.safety_forms import GeneralManagerSafetyReportForm
+from ..services.safety_services import (
+    assign_user,
+    get_sign_users,
+    update_safety_general,
+)
+from ..utils.send_alert import email_send
 
 
 @login_required(login_url="/user/login/")
 def safety(request):
-    return render(request, "work/safety/safety.html")
+    page = request.GET.get("page", 1)
+
+    safety_list = SafetyReport.objects.all().order_by("-isCheckManager", "-docNum")
+
+    paginator = Paginator(safety_list, 10)
+    page_obj = paginator.get_page(page)
+
+    return render(request, "work/safety/safety.html", {"safetyitems": page_obj})
 
 
 @login_required(login_url="/user/login/")
@@ -20,6 +37,7 @@ def create_safety(request):
             safety.writerId = request.user
             files = request.POST.getlist("docs[]")
             safety.save()
+            safety.docs.clear()
             for file_id in files:
                 doc_file = DocsFile.objects.get(pk=int(file_id))
                 safety.docs.add(doc_file)
@@ -56,20 +74,29 @@ def create_safety(request):
 
 @login_required(login_url="/user/login/")
 def update_safety(request, pk):
-    if request.method == "POST":
-        form = GeneralManagerSafetyReportForm(request.POST)
-        if form.is_valid():
-            safety = form.save(commit=False)
-            safety.save()
-            return redirect("work:update_safety", safety.docNum)
-    doc = SafetyReport.objects.get(docNum=pk)
-    form = GeneralManagerSafetyReportForm(instance=doc)
-    return render(request, "work/safety/create_safety_general.html", {"form": form})
+    if request.user.class2 == "일반 관리자":
+        return update_safety_general(request, pk)
+    # 각 관리자 별로 IF 문 생성
+    return Http404("잘못된 접근입니다.")
 
 
 @login_required(login_url="/user/login/")
-def require_sign(request, docNum):
-    return render(request, "work/safety/update_safety.html", {"docNum": docNum})
+def get_users(request):
+    users = get_sign_users(request)
+    return JsonResponse(
+        {"users": list(users.values("id", "name"))},
+        json_dumps_params={"ensure_ascii": False},
+    )
+
+
+# 메일 전송 및 문자 전송
+@login_required(login_url="/user/login/")
+def require_sign(request):
+    if request.method == "POST":
+        email_send(int(request.POST.get("sign")))
+        assign_user(int(request.POST.get("docNum")), int(request.POST.get("sign")))
+        return redirect("work:safety")
+    return Http404("잘못된 접근입니다.")
 
 
 @login_required(login_url="/user/login/")
