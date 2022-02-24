aa205faa3f97ab799d883ae2925f9444f36f4b35
seokmin <soos3121@gmail.com>
2022-02-24 16:52:33 +0900

    feat : 일반 건설사업관리기술인 form 제작

--- work/forms/safety_forms.py
+++ work/forms/safety_forms.py
@@ -7,3 +7,9 @@ class GeneralManagerSafetyReportForm(forms.ModelForm):
     class Meta:
         model = SafetyReport
         fields = ["docNum", "date", "title", "constructType", "text"]
+
+
+class GeneralEngineerSafetyReportForm(forms.ModelForm):
+    class Meta:
+        model = SafetyReport
+        fields = ["replyDate", "generalEngineerText", "result"]
