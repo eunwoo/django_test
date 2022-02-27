53e6f7d702395b67e0044de996c58a379361ccac
decyma <soos3121@gmail.com>
2022-02-27 15:51:07 +0900

    feat : material service 추가

--- work/services/material_services.py
+++ work/services/material_services.py
@@ -1,17 +1,125 @@
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 from django.shortcuts import render, redirect
 
-from ..models import MaterialSupplyReport
+from system_manager.models import DocsFile
+from work.utils.send_alert import email_send
+
+from ..models import MaterialSupplyReport, SupplyList
 from ..forms.material_forms import GeneralManagerMaterialSupplyReportForm
 
 
+def get_material_list_by_user(user):
+    if user.class2 == "일반 관리자":
+        return MaterialSupplyReport.objects.filter(writerId=user).order_by(
+            "-isCheckManager", "-docNum"
+        )
+    elif user.class2 == "현장 대리인":
+        return MaterialSupplyReport.objects.filter(agentId=user).order_by(
+            "-isReadAgent", "-isCheckAgent", "-docNum"
+        )
+    elif user.class2 == "일반 건설사업관리기술인":
+        return MaterialSupplyReport.objects.filter(generalEngineerId=user).order_by(
+            "-isReadGeneralEngineer", "-isCheckGeneralEngineer", "-docNum"
+        )
+    else:
+        return MaterialSupplyReport.objects.filter(totalEngineerId=user).order_by(
+            "-isReadTotalEngineer", "-isSuccess", "-docNum"
+        )
+
+
 def create_material_service(request):
-    form = GeneralManagerMaterialSupplyReportForm()
+    if request.method == "POST":
+        form = GeneralManagerMaterialSupplyReportForm(request.POST, request.FILES)
+        if form.is_valid():
+            material = form.save(commit=False)
+            material.writerId = request.user
+            files = request.POST.getlist("docs[]")
+            supply_comp = request.POST.getlist("supply_comp[]")
+            supply_type = request.POST.getlist("supply_type[]")
+            supply_size = request.POST.getlist("supply_size[]")
+            supply_etc = request.POST.getlist("supply_etc[]")
+            material.save()
+            for file_id in files:
+                doc_file = DocsFile.objects.get(pk=int(file_id))
+                material.docs.add(doc_file)
+            for index in range(len(supply_comp)):
+                supply_list = SupplyList.objects.create(
+                    name=supply_comp[index],
+                    goods=supply_type[index],
+                    size=supply_size[index],
+                    etc=supply_etc[index],
+                    materialSupplyReportId=material,
+                )
+                supply_list.save()
+            return redirect("work:update_material", material.docNum)
+    else:
+        form = GeneralManagerMaterialSupplyReportForm()
     last_doc = MaterialSupplyReport.objects.last()
     if not last_doc:
         docNum = 1
     else:
         docNum = last_doc.docNum + 1
+    accept_docs = DocsFile.objects.filter(type="가설기자재 공급원 승인 문서")
     return render(
-        request, "work/material/create_material.html", {"form": form, "docNum": docNum}
+        request,
+        "work/material/create_material.html",
+        {"form": form, "docNum": docNum, "accept_docs": accept_docs},
     )
+
+
+def update_material_service(request, docNum):
+    instance = MaterialSupplyReport.objects.get(docNum=docNum)
+    if request.method == "POST":
+        form = GeneralManagerMaterialSupplyReportForm(request.POST, instance=instance)
+        if form.is_valid():
+            material = form.save(commit=False)
+            material.writerId = request.user
+            files = request.POST.getlist("docs[]")
+            supply_comp = request.POST.getlist("supply_comp[]")
+            supply_type = request.POST.getlist("supply_type[]")
+            supply_size = request.POST.getlist("supply_size[]")
+            supply_etc = request.POST.getlist("supply_etc[]")
+            material.save()
+            material.docs.clear()
+            material.supply_list.clear()
+            for file_id in files:
+                doc_file = DocsFile.objects.get(pk=int(file_id))
+                material.docs.add(doc_file)
+            for index in range(len(supply_comp)):
+                supply_list = SupplyList.objects.create(
+                    name=supply_comp[index],
+                    goods=supply_type[index],
+                    size=supply_size[index],
+                    etc=supply_etc[index],
+                )
+                supply_list.save()
+                material.supply_list.add(supply_list)
+            return redirect("work:update_material", material.docNum)
+    else:
+        form = GeneralManagerMaterialSupplyReportForm(instance=instance)
+    accept_docs = DocsFile.objects.filter(type="가설기자재 공급원 승인 문서")
+    supply_origins = instance.supply_list.all()
+    return render(
+        request,
+        "work/material/create_material.html",
+        {
+            "form": form,
+            "docNum": docNum,
+            "material": instance,
+            "accept_docs": accept_docs,
+            "supply_origins": supply_origins,
+        },
+    )
+
+
+def read_material_service(user, pk):
+    material = MaterialSupplyReport.objects.get(pk=pk)
+    if user.class2 == "일반 관리자":
+        material.isCheckManager = True
+    elif user.class2 == "현장 대리인":
+        material.isCheckAgent = True
+    elif user.class2 == "일반 건설사업관리기술인":
+        material.isCheckGeneralEngineer = True
+    material.save()
+    return material
