2ad547c76a1c89600ce7cdb2af41d6374e1e8a7a
seokmin <soos3121@gmail.com>
2022-02-28 16:37:13 +0900

    feat : 품질검사 의뢰서 작성 뷰 추가

--- work/views/quality_request_views.py
+++ work/views/quality_request_views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
+from ..services.quality_request_services import create_quality_request_service
+
 
 @login_required(login_url="/user/login/")
 def quality_request(request):
@@ -9,7 +11,7 @@ def quality_request(request):
 
 @login_required(login_url="/user/login/")
 def create_quality_request(request):
-    return render(request, "work/quality/quality_request/create_quality_request.html")
+    return create_quality_request_service(request)
 
 
 @login_required(login_url="/user/login/")
