22913f2d59058e023e7ce5f4e3481c508d4836a7
seokmin <soos3121@gmail.com>
2022-02-28 16:59:03 +0900

    feat : 설치위치명 url, view 추가

--- system_manager/views/locate_views.py
+++ system_manager/views/locate_views.py
@@ -1,7 +1,13 @@
 from django.contrib.auth.decorators import login_required
+from django.http import Http404, JsonResponse
 from django.shortcuts import render, redirect
 
-from ..models import LocateClass, InstallLocate
+from ..models import (
+    LocateClass,
+    InstallLocate,
+    InstallLocateClass2,
+    InstallLocateClass1,
+)
 
 
 @login_required(login_url="/user/login/")
@@ -29,3 +35,24 @@ def locate_class(request, pk):
     if not request.user.is_system_manager:  # 시스템 매니저만 접근 가능
         return redirect("main:home")
     return redirect("main:home")
+
+
+@login_required(login_url="/user/login/")
+def read_locate_class(request, class_type):
+    class_id = request.GET.get("class_id", 1)
+    if class_type == 1:
+        install_locate = InstallLocateClass1.objects.all()
+    elif class_type == 2:
+        install_locate = InstallLocateClass2.objects.filter(
+            class1=InstallLocateClass1.objects.get(pk=class_id)
+        )
+    elif class_type == 3:
+        install_locate = InstallLocate.objects.filter(
+            class2=InstallLocateClass2.objects.get(pk=class_id)
+        )
+    else:
+        return Http404()
+    return JsonResponse(
+        {"install_locate": list(install_locate.values())},
+        json_dumps_params={"ensure_ascii": False},
+    )
