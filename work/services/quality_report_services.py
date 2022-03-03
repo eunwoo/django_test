bb5803399cd0772504d5255509a3eec29ff0a189
seokmin <soos3121@gmail.com>
2022-03-03 17:00:06 +0900

    feat : 품질검사 보고서 완료

--- work/services/quality_report_services.py
+++ work/services/quality_report_services.py
@@ -11,6 +11,37 @@ from ..models import (
 from ..forms.quality_report_forms import GeneralManagerQualityPerformanceReportForm
 
 
+def get_qty_report_list_by_user(user):
+    if user.class2 == "일반 관리자":
+        return QualityPerformanceReport.objects.filter(writerId=user).order_by(
+            "isCheckManager", "-isSuccess", "-docNum"
+        )
+    elif user.class2 == "현장 대리인":
+        return QualityPerformanceReport.objects.filter(agentId=user).order_by(
+            "isCheckAgent", "-isSuccess", "-docNum"
+        )
+    elif user.class2 == "일반 건설사업관리기술인":
+        return QualityPerformanceReport.objects.filter(generalEngineerId=user).order_by(
+            "isCheckGeneralEngineer", "-isSuccess", "-docNum"
+        )
+    else:
+        return QualityPerformanceReport.objects.filter(totalEngineerId=user).order_by(
+            "isSuccess", "-docNum"
+        )
+
+
+def read_qty_report_service(user, pk):
+    qty_report = QualityPerformanceReport.objects.get(docNum=pk)
+    if user.class2 == "일반 관리자":
+        qty_report.isCheckManager = True
+    elif user.class2 == "현장 대리인":
+        qty_report.isCheckAgent = True
+    elif user.class2 == "일반 건설사업관리기술인":
+        qty_report.isCheckGeneralEngineer = True
+    qty_report.save()
+    return qty_report
+
+
 def create_quality_report_service(request):
     field = Field.objects.get(pk=1)  # 현장 관리는 하나만 있음
     last_doc = QualityPerformanceReport.objects.last()
@@ -22,7 +53,7 @@ def create_quality_report_service(request):
             qty_report.fieldId = field
             qty_report.save()
             create_quality_performance(request, qty_report)
-            files = request.FILES.getlist("doc_files")
+            files = request.FILES.getlist("docs_files")
             for file in files:
                 docs_file = QualityPerformanceFile.objects.create(
                     title=file.name, doc=file, quality_performance_report_id=qty_report
@@ -43,6 +74,19 @@ def create_quality_report_service(request):
 
 
 def update_quality_report_service(request, pk):
+    if request.user.class2 == "일반 관리자":
+        return update_quality_report_general(request, pk)
+    elif request.user.class2 == "현장 대리인":
+        return update_quality_report_agent(request, pk)
+    elif request.user.class2 == "일반 건설사업관리기술인":
+        return update_quality_report_generalEngineer(request, pk)
+    elif request.user.class2 == "총괄 건설사업관리기술인":
+        return update_quality_report_totalEngineer(request, pk)
+    else:
+        return Http404()
+
+
+def update_quality_report_general(request, pk):
     instance = QualityPerformanceReport.objects.get(docNum=pk)
     if request.method == "POST":
         form = GeneralManagerQualityPerformanceReportForm(
@@ -51,11 +95,11 @@ def update_quality_report_service(request, pk):
         if form.is_valid():
             qty_report = form.save(commit=False)
             qty_report.writerId = request.user
-            qty_report.quality_performance.clear()
-            qty_report.quality_performance_file.clear()
             qty_report.save()
+            qty_report.quality_performance_file.all().delete()
+            qty_report.quality_performance.all().delete()
             create_quality_performance(request, qty_report)
-            files = request.FILES.getlist("doc_files")
+            files = request.FILES.getlist("docs_files")
             for file in files:
                 docs_file = QualityPerformanceFile.objects.create(
                     title=file.name, doc=file, quality_performance_report_id=qty_report
@@ -68,6 +112,33 @@ def update_quality_report_service(request, pk):
         request,
         "work/quality/quality_report/create_quality_report.html",
         {"form": form, "docNum": instance.docNum, "field": instance.fieldId},
+    )
+
+
+def update_quality_report_agent(request, pk):
+    qty_report = QualityPerformanceReport.objects.get(docNum=pk)
+    return render(
+        request,
+        "work/quality/quality_report/update_quality_report_agent.html",
+        {"qty_report": qty_report},
+    )
+
+
+def update_quality_report_generalEngineer(request, pk):
+    qty_report = QualityPerformanceReport.objects.get(docNum=pk)
+    return render(
+        request,
+        "work/quality/quality_report/update_quality_report_generalEngineer.html",
+        {"qty_report": qty_report},
+    )
+
+
+def update_quality_report_totalEngineer(request, pk):
+    qty_report = QualityPerformanceReport.objects.get(docNum=pk)
+    return render(
+        request,
+        "work/quality/quality_report/update_quality_report_totalEngineer.html",
+        {"qty_report": qty_report},
     )
 
 
@@ -108,3 +179,13 @@ def create_quality_performance(request, qty_report):
             quality_performance_report_id=qty_report,
         )
         performance.save()
+
+
+def qty_report_success(docNum: int):
+    qty_report = QualityPerformanceReport.objects.get(docNum=docNum)
+    qty_report.isSuccess = True
+    qty_report.isCheckManager = False
+    qty_report.isCheckAgent = False
+    qty_report.isCheckGeneralEngineer = False
+    qty_report.save()
+    return True
