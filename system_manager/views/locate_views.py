a251733250a990d165201e2f77ce8ccc8daf965d
seokmin <soos3121@gmail.com>
2022-03-03 16:58:46 +0900

    feat : locate view 추가

--- system_manager/views/locate_views.py
+++ system_manager/views/locate_views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.decorators import login_required
-from django.http import Http404, JsonResponse
+from django.http import Http404, HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
+from django.core import serializers
 
 from ..models import (
-    LocateClass,
     InstallLocate,
     InstallLocateClass2,
     InstallLocateClass1,
@@ -14,17 +14,13 @@ from ..models import (
 def apply_locate(request):
     if not request.user.is_system_manager:  # 시스템 매니저만 접근 가능
         return redirect("main:home")
-    class1 = LocateClass.objects.filter(type="class1")
-    class2 = LocateClass.objects.filter(type="class2")
-    class3 = LocateClass.objects.filter(type="class3")
+    class1 = InstallLocateClass1.objects.all()
     install_locate = InstallLocate.objects.all()
     return render(
         request,
         "system_manager/apply_locate.html",
         {
             "class1": class1,
-            "class2": class2,
-            "class3": class3,
             "install_locate": install_locate,
         },
     )
@@ -56,3 +52,45 @@ def read_locate_class(request, class_type):
         {"install_locate": list(install_locate.values())},
         json_dumps_params={"ensure_ascii": False},
     )
+
+
+@login_required(login_url="/user/login/")
+def add_locate(request):
+    if request.method == "POST":
+        class_type = int(request.POST.get("class_type"))
+        if class_type == 1:
+            target_class = InstallLocateClass1(
+                class1=request.POST.get("name"),
+            )
+            target_class.save()
+        elif class_type == 2:
+            target_class = InstallLocateClass2(
+                class2=request.POST.get("name"),
+                class1=InstallLocateClass1.objects.get(pk=request.POST.get("class_id")),
+            )
+            target_class.save()
+        elif class_type == 3:
+            target_class = InstallLocate(
+                class3=request.POST.get("name"),
+                class2=InstallLocateClass2.objects.get(pk=request.POST.get("class_id")),
+            )
+            target_class.save()
+        return JsonResponse(
+            {"result": "success", "data": serializers.serialize("json", [target_class])}
+        )
+    else:
+        return Http404()
+
+
+@login_required(login_url="/user/login/")
+def delete_locate(request):
+    if request.method == "POST":
+        class_type = int(request.POST.get("class_type"))
+        if class_type == 1:
+            InstallLocateClass1.objects.get(pk=request.POST.get("class_id")).delete()
+        elif class_type == 2:
+            InstallLocateClass2.objects.get(pk=request.POST.get("class_id")).delete()
+        elif class_type == 3:
+            InstallLocate.objects.get(pk=request.POST.get("class_id")).delete()
+        return HttpResponse(status=200)
+    return Http404()
