39c9339cfa9372db66e3cdd521eaba8d54c95421
decyma <soos3121@gmail.com>
2022-03-05 15:59:22 +0900

    feat : 설치작업전 체크리스트 폼 생성

---
+++ work/forms/before_install_form.py
@@ -0,0 +1,12 @@
+from django import forms
+
+from ..models import BeforeInstallCheckList
+
+
+class BeforeInstallCheckListForm(forms.ModelForm):
+    class Meta:
+        model = BeforeInstallCheckList
+        fields = [
+            "date",
+            "detailLocate",
+        ]
