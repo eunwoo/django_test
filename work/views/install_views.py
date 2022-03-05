11f1aa901e5b5f04b43d3b362de6f9d5512a5757
decyma <soos3121@gmail.com>
2022-03-05 15:59:34 +0900

    feat : install view 추가

--- work/views/install_views.py
+++ work/views/install_views.py
@@ -1,8 +1,14 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from system_manager.models import InstallLocate
+from work.forms.before_install_form import BeforeInstallCheckListForm
 
-from work.models import BeforeInspectionItem, BeforeInstallCheckList
+from work.models import (
+    BeforeInspectionItem,
+    BeforeInspectionResult,
+    BeforeInstallCheckList,
+)
 
 
 @login_required(login_url="/user/login/")
@@ -43,12 +49,45 @@ def before_install_checklist(request, type: str):
         equipment = "2"
     else:
         equipment = "3"
+    if request.method == "POST":
+        form = BeforeInstallCheckListForm(request.POST)
+        if form.is_valid():
+            before_checklist = form.save(commit=False)
+            before_checklist.equipment = type
+            before_checklist.writerId = request.user
+            before_checklist.locateId = InstallLocate.objects.get(
+                pk=request.POST["locate"]
+            )
+            pk_checklist = request.POST.getlist("checklist-pk")
+            image_keys = request.FILES.keys()
+            before_checklist.save()
+            for pk in pk_checklist:
+                result_item = BeforeInspectionResult(
+                    result=request.POST[pk],
+                    before_install_checklist_id=before_checklist,
+                    before_inspection_item_id=BeforeInspectionItem.objects.get(pk=pk),
+                )
+                result_item.content = request.POST[f"{pk}-belong"]
+                result_item.save()
+                if f"{pk}-images[]" in image_keys:
+                    images = request.FILES.getlist(f"{pk}-images[]")
+                    for img in images:
+                        result_item.measures.create(img=img)
+    else:
+        form = BeforeInstallCheckListForm()
     checklist = BeforeInspectionItem.objects.filter(equipment=equipment)
+    last_doc = BeforeInstallCheckList.objects.last()
+    if not last_doc:
+        docNum = 1
+    else:
+        docNum = last_doc.pk + 1
     return render(
         request,
         "work/install/before/before_install_checklist.html",
         {
             "type": type,
             "checklist": checklist,
+            "docNum": docNum,
+            "form": form,
         },
     )
