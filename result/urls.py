718d06c293529c94d83ab177341b4723dfccd01f
seokmin <soos3121@gmail.com>
2022-03-11 14:38:51 +0900

    feat : 결과보고 구현

--- result/urls.py
+++ result/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 app_name = "result"
 
-urlpatterns = [path("result/", views.search, name="search")]
+urlpatterns = [
+    path("result/", views.search, name="search"),
+    path("search/", views.search_result, name="search_result"),
+]
--- result/views.py
+++ result/views.py
@@ -1,52 +1,51 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.db.models import CharField, Value
-from django.db.models import F
+from django.core.paginator import Paginator
+from urllib import parse
 
 # Create your views here.
 
-
-from work.models import (
-    SafetyReport,
-    MaterialSupplyReport,
-    QualityInspectionRequest,
-    QualityPerformanceReport,
-    BeforeInstallCheckList,
-    InstallCheckList,
-)
+from .services import get_search_list, custom_redirect
 
 
 @login_required(login_url="/user/login/")
 def search(request):
-    safety = (
-        SafetyReport.objects.all()
-        .values_list("docNum", "created_at")
-        .annotate(type=Value("safety", output_field=CharField()))
-    )
-    material = (
-        MaterialSupplyReport.objects.all()
-        .values_list("docNum", "created_at")
-        .annotate(type=Value("material", output_field=CharField()))
-    )
-    quality = (
-        QualityInspectionRequest.objects.all()
-        .values_list("docNum", "created_at")
-        .annotate(type=Value("qty_request", output_field=CharField()))
-    ).union(
-        (
-            QualityPerformanceReport.objects.all()
-            .values_list("docNum", "created_at")
-            .annotate(type=Value("qty_report", output_field=CharField()))
+    if request.GET.getlist("query"):
+        return custom_redirect(
+            "result:search_result",
+            query=request.GET.getlist("query"),
+            locate=request.GET.get("locate", 0),
         )
-    )
-    checklist = (
-        BeforeInstallCheckList.objects.all()
-        .annotate(docNum=F("pk"))
-        .values_list("docNum", "created_at")
-        .annotate(type=Value("checklist", output_field=CharField()))
-    )
-    asdf = safety.union(checklist)
     return render(
         request,
         "result/search.html",
+    )
+
+
+@login_required(login_url="/user/login/")
+def search_result(request):
+    page = request.GET.get("page", 1)
+
+    result_list = get_search_list(
+        request.GET.getlist("query"), request.GET.get("locate", 0)
+    )
+
+    paginator = Paginator(result_list, 10)
+    page_obj = paginator.get_page(page)
+
+    query = parse.urlencode(
+        {
+            "query": request.GET.getlist("query"),
+            "locate": request.GET.get("locate", 0),
+        },
+        True,  # True 설정시 ?query=[1,2,3,4]가 아닌 ?query=1&query=2&query=3&query=4 으로 인코딩됨
+    )
+
+    return render(
+        request,
+        "result/search_result.html",
+        {
+            "items": page_obj,
+            "query": f"&{query}",
+        },
     )
