6e2398bdd6d740fe20295162a0d9adc86f1a4b95
decyma <soos3121@gmail.com>
2022-04-20 11:17:29 +0900

    feat : 공지사항 삭제기능 추가

--- announcement/urls.py
+++ announcement/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("create/", views.create_views, name="create_announcement"),
     path("update/<int:pk>/", views.update_views, name="update_announcement"),
     path("read/<int:pk>/", views.read_views, name="read_announcement"),
+    path("delete/", views.delete_announcements, name="delete_announcement"),
     path("", views.get_announcements, name="announcement"),
 ]
