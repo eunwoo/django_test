64273486f00eac197709e7ec8bae81eb6d60bcac
decyma <soos3121@gmail.com>
2022-06-10 10:09:54 +0900

    refactor : 메인화면 주석 추가

--- work/views/main_views.py
+++ work/views/main_views.py
@@ -4,12 +4,14 @@ from django.shortcuts import render, redirect
 from work.services.main_services import get_unread_docs_list, get_unread_quality_docs
 
 
+# 업무 메인페이지
 @login_required(login_url="/user/login/")
 def index(request):
     context = get_unread_docs_list(request.user)
     return render(request, "work/main.html", context)
 
 
+# 품질검사 선택페이지
 @login_required(login_url="/user/login/")
 def quality_menu(request):
     context = get_unread_quality_docs(request.user)
