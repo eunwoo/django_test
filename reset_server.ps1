82878d05b5040450dbb2df32652c182ffc68c9f1
decyma <soos3121@gmail.com>
2022-06-27 16:00:51 +0900

    chore : 윈도우 powershell 설정

--- reset_server.ps1
+++ reset_server.ps1
@@ -1,4 +1,4 @@
-source ./.venv/bin/activate
+.venv/Scripts/Activate.ps1
 Remove-Item "./db.sqlite3" -Force
 python manage.py migrate
 python manage.py loaddata ./init_data.json
--- run.ps1
+++ run.ps1
@@ -1,2 +1,2 @@
-source ./.venv/bin/activate
+.venv/Scripts/Activate.ps1
 python manage.py runserver 0.0.0.0:80
