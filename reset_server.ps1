23766eae418f70f1512a5fecad07089c6f1cae0e
decyma <soos3121@gmail.com>
2022-06-28 11:45:59 +0900

    chore : reset_server 추가

--- reset_server.ps1
+++ reset_server.ps1
@@ -1,4 +1,5 @@
 .venv/Scripts/Activate.ps1
 Remove-Item "./db.sqlite3" -Force
-python manage.py migrate
-python manage.py loaddata ./init_data.json
+python manage.py makemigrations
+python manage.py migrate --run-sync
+python manage.py loaddata ./init_data.yaml
